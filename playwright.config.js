010ecb38d489330ab728266097ab1601b6c289fe
Sitifatimoh <fatimahetae@gmail.com>
2025-08-27 10:21:56 +0700

    New CI workflow03

--- playwright.config.js
+++ playwright.config.js
@@ -26,12 +26,7 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: [
-    ["allure-playwright"],
-    ["html"],
-    ["list"],
-    ["json", { outputFile: "json-results/results.json" }],
-  ],
+  reporter: [["line"], ["allure-playwright"]],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
